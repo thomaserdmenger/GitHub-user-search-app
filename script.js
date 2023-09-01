1e28f20b1e606263e0508abea70e199cd2be00a8
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-09-01 08:50:08 +0200

    Refactor dark/light theme

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-export let userNameInput = "";
-
 import getData from "./modules/fetchData.js";
 import createHoverEffect from "./modules/themeHoverEffect.js";
+import { darkAttributes, lightAttributes } from "./modules/themeAttributes.js";
 
-import {
-  calculateSettingAsThemeString,
-  localStorageTheme,
-  systemSettingLight,
-} from "./modules/themeSettingCalculate.js";
-
-import { themeSwitchOnClick } from "./modules/themeSettingCalculate.js";
+export let userNameInput = "";
 
 // GLOBAL
 const form = document.querySelector(".search-form");
@@ -30,8 +23,98 @@ form.addEventListener("submit", (e) => {
 // Add hover state on mode toggle on header
 createHoverEffect();
 
-// temp
+/** Get DOM Elements */
 
-calculateSettingAsThemeString(localStorageTheme, systemSettingLight);
+/** Utility function to calculate the current theme setting.
+ *  Look for a local storage value.
+ *  Fall back to system setting.
+ *  Fall back to light mode.
+ */
 
-themeSwitchOnClick();
+function calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+}) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+/**
+ *  Utility function to update the button text, button icon and aria-label
+ */
+
+function updateButton({ buttonEl, buttonLogo, isDark }) {
+  const newCta = isDark ? "light" : "dark";
+  buttonEl.setAttribute("aria-label", newCta);
+  buttonEl.textContent = newCta;
+  isDark
+    ? buttonLogo.setAttribute("d", darkAttributes.d)
+    : buttonLogo.setAttribute("d", lightAttributes.d);
+}
+
+/**
+ * Utility function to update the theme setting on the html tag
+ */
+
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+/**
+ * On page load:
+ */
+
+/**
+ * 1. Grab what we nee from the DOM and system settings on page load
+ */
+
+const button = document.querySelector("[data-theme-toggle]");
+const buttonText = document.querySelector(".header-mode-button-text");
+const buttonIcon = document.querySelector(".header-mode-image");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+/**
+ * 2. Work out the current site setting
+ */
+
+let currentThemeSetting = calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+});
+
+/**
+ * 3. Update the theme setting and button text according to current settings
+ */
+
+updateButton({
+  buttonEl: buttonText,
+  buttonLogo: buttonIcon,
+  isDark: currentThemeSetting === "dark",
+});
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+ * 4. Add an event listener to toggle the theme
+ */
+
+button.addEventListener("click", () => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({
+    buttonEl: buttonText,
+    buttonLogo: buttonIcon,
+    isDark: newTheme === "dark",
+  });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+});
